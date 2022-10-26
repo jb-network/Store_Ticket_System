d6c4c11b7dc26860b46791492d2a8b38194b8610
jb-network <85036487+jb-network@users.noreply.github.com>
2022-10-26 12:29:39 -0500

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -1,23 +1,39 @@
 from os import system
 
+receipt_boundary = "-" * 50
+
+def medicine():
+    for number in range(1, 1000):
+        yield f'M - {number}'
+
+def perfumes():
+    for number in range(1, 1000):
+        yield f'P - {number}'
+
+def cosmetics():
+    for number in range(1, 1000):
+        yield f'C - {number}'
+
+m = medicine()
+p = perfumes()
+c = cosmetics()
+
+# Python Decorator
 def customer_ticket(user_selection):
     system("cls")
-    def store_banner():
-        system("cls")
-        print("Sweet's Emporium")
-        print("Your Number is: ")
-        user_selection()
-        print("Someone will be with you shortly")
-    return store_banner
-
-@customer_ticket
-def medicine():
-    print("Medicine number")
-
-@customer_ticket
-def perfumes():
-    print("Perfume number")
-
-@customer_ticket
-def cosmetics():
-    print("Cosmetic number")
+    print(receipt_boundary)
+    print("Sweet's Emporium ticketing system")
+    print(receipt_boundary)
+    print("Your ticket number is: ")
+    if user_selection == 'm':
+        print(next(m))
+    elif user_selection == 'p':
+        print(next(p))
+    else:
+        print(next(c))
+    print("Someone from that section will be with you shortly")
+    print(receipt_boundary)
+    print("Press 'Enter' to print your ticket")
+    print(receipt_boundary)
+    input()
+    system('cls')
